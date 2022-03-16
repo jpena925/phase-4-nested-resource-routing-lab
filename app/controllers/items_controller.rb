786c2140ed2070b14031c5c41d62fa61a7a47de7
Jack Pena <penajackc@gmail.com>
2022-03-16 11:25:02 -0700

    commit tree b9c639a2b62eed598bcdb0275dbf24ca7a422b6f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :no_record_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, only: [:id, :name, :description, :price, :user_id]
+  end
+
+  def create
+    item = Item.create(items_params)
+    user = User.find(params[:user_id])
+    user.items << item
+    render json: item, status: :created
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :description, :price)
+  end
+
+  def no_record_found
+    render json: {error: "User not found"}, status: :not_found
   end
 
 end
